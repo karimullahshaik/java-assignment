7f5df8005b335a4d272e7626d6369f83f657137d
Karimullah.Shaik <Karimullah.Shaik@axiatadigitallabs.com>
2022-05-05 00:07:17 +0800

    Added improved loggers and code refactoring

--- src/main/java/com/maybank/wordcount/util/WordCountUtil.java
+++ src/main/java/com/maybank/wordcount/util/WordCountUtil.java
@@ -2,16 +2,27 @@ package com.maybank.wordcount.util;
 
 /**
  * 
- * @Author: Karimullah Shaik 
+ * @Author: Karimullah Shaik Utility Class for Word Count
  * 
  */
 public class WordCountUtil {
-	
+
+	/**
+	 * 
+	 * @param text
+	 * @return boolean (true -> if the text is null or empty; false -> if the text
+	 *         is not empty)
+	 */
 	public static boolean checkForEmptyString(final String text) {
 		return text == null || text.isEmpty();
 	}
-	
-	
+
+	/**
+	 * 
+	 * @param text
+	 * @return boolean (true -> if text param have characters containing the regex
+	 *         a-z, A-Z, . and -)
+	 */
 	public static boolean isWord(final String text) {
 		return text.matches("[a-zA-Z]+");
 	}
